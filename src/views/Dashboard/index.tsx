14d8843867fae2e6522813f7b050d87a7d535528
gv4383 <gregory.vargas18@gmail.com>
2020-05-17 18:32:47 -0500

    Use hooks for selectors and actions

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,42 +1,21 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Dashboard from './presenter';
-import { AppState } from '../../store';
 import { getUsers } from '../../store/actions/users';
-import { users } from '../../store/selectors/users';
-// import { User } from '../../store/reducers/users/types';
+import { users as UsersSelector } from '../../store/selectors/users';
 
-interface Props {
-  getUsers: typeof getUsers;
-  // users: any;
-}
-
-const DashboardContainer = (props: Props) => {
-  const { getUsers } = props;
+const DashboardContainer = () => {
+  const dispatch = useDispatch();
+  const users = useSelector(UsersSelector);
 
   useEffect(() => {
-    getUsers();
-  }, [getUsers]);
+    dispatch(getUsers());
+  }, [dispatch, getUsers]);
 
   return (
-    <Dashboard />
+    <Dashboard users={users} />
   );
 };
 
-const mapStateToProps = (state: AppState) => ({
-  users: users(state),
-});
-
-const mapDispatchToProps = (dispatch: any) => bindActionCreators(
-  {
-    getUsers,
-  },
-  dispatch,
-);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(DashboardContainer);
+export default DashboardContainer;
--- src/views/Dashboard/presenter.tsx
+++ src/views/Dashboard/presenter.tsx
@@ -1,9 +1,20 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 
 import UserCard from '../../components/UserCard';
 
-const Dashboard: FunctionComponent = () => (
-  <UserCard />
-);
+interface Props {
+  users: any;
+}
+
+const Dashboard = (props: Props) => {
+  const { users } = props;
+  const displayUsers = users?.data && users.data.map((user: any) => <UserCard key={user.name} />);
+
+  return (
+    <div>
+      { displayUsers }
+    </div>
+  );
+};
 
 export default Dashboard;
