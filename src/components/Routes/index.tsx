6bcbfdcf713204d3251ad511fbd4d15d57053237
gv4383 <gregory.vargas18@gmail.com>
2020-05-24 19:35:06 -0500

    Update profile route

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -7,7 +7,7 @@ import Profile from '../../views/Profile';
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Dashboard} />
-    <Route path="/profile" component={Profile} />
+    <Route path="/profile/:id" component={Profile} />
   </Switch>
 );
 
