d48d33054d919951d78cb750e37e5ce2b9926574
gv4383 <gregory.vargas18@gmail.com>
2020-05-24 19:43:02 -0500

    Dispatch getUser in ProfileContainer

--- src/views/Profile/index.tsx
+++ src/views/Profile/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Profile from './presenter';
+import { getUser } from '../../store/actions/user';
+import { user as UserSelector } from '../../store/selectors/user';
 
-const ProfileContainer = () => <Profile />;
+const ProfileContainer = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(UserSelector);
+
+  useEffect(() => {
+    dispatch(getUser(1));
+  }, [dispatch]);
+
+  return <Profile />;
+};
 
 export default ProfileContainer;
