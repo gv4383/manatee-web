9acc32608599cfde2430551d2bafbf216cba11ee
gv4383 <gregory.vargas18@gmail.com>
2020-05-24 20:58:54 -0500

    Remove catch methods in Api object

--- src/store/utils/api/index.ts
+++ src/store/utils/api/index.ts
@@ -6,27 +6,23 @@ const API = {
   get: (endpoint: string) => axios(`${baseUrl}${endpoint}`, {
     method: 'GET',
   })
-    .then(res => res.data)
-    .catch(err => err),
+    .then(res => res.data),
   post: (endpoint: string, payload: any) => axios(`${baseUrl}${endpoint}`, {
     method: 'POST',
     headers: null,
     data: JSON.stringify(payload),
   })
-    .then(res => res)
-    .catch(err => err),
+    .then(res => res),
   put: (endpoint: string, payload: any) => axios(`${baseUrl}${endpoint}`, {
     method: 'POST',
     headers: null,
     data: JSON.stringify(payload),
   })
-    .then(res => res)
-    .catch(err => err),
+    .then(res => res),
   delete: (endpoint: string) => axios(`${baseUrl}${endpoint}`, {
     method: 'DELETE',
   })
-    .then(res => res)
-    .catch(err => err),
+    .then(res => res),
 };
 
 export default API;
