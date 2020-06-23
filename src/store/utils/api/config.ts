e823c058e04b4fb842ff6e9b0f1acc8d9c68543e
gv4383 <gregory.vargas18@gmail.com>
2020-06-23 00:44:01 -0500

    Send headers with API requests

--- src/store/utils/api/config.ts
+++ src/store/utils/api/config.ts
@@ -1,2 +1,8 @@
-const baseUrl = 'http://localhost:5000/api';
-export default baseUrl;
+import { HttpHeaders } from './types';
+
+export const baseUrl = 'http://localhost:5000/api';
+
+export const getHeaders = (): HttpHeaders<string> => ({
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+});
--- src/store/utils/api/index.ts
+++ src/store/utils/api/index.ts
@@ -1,24 +1,28 @@
 import axios from 'axios';
 
-import baseUrl from './config';
+import { baseUrl, getHeaders } from './config';
 
 const API = {
   get: (endpoint: string) => axios(`${baseUrl}${endpoint}`, {
     method: 'GET',
+    headers: getHeaders(),
   })
     .then(res => res.data),
   post: (endpoint: string, payload: any) => axios(`${baseUrl}${endpoint}`, {
     method: 'POST',
+    headers: getHeaders(),
     data: payload,
   })
     .then(res => res),
   put: (endpoint: string, payload: any) => axios(`${baseUrl}${endpoint}`, {
     method: 'POST',
+    headers: getHeaders(),
     data: payload,
   })
     .then(res => res),
   delete: (endpoint: string) => axios(`${baseUrl}${endpoint}`, {
     method: 'DELETE',
+    headers: getHeaders(),
   })
     .then(res => res),
 };
