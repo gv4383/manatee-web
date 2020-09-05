d2b0ec0e077fc72ef0a3ece950d131ee302a612b
gv4383 <gregory.vargas18@gmail.com>
2020-09-05 14:19:20 -0500

    Create basic search component

--- src/views/Match/index.tsx
+++ src/views/Match/index.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, {
+  ChangeEvent,
+  FormEvent,
+  useState,
+} from 'react';
+import { InputOnChangeData } from 'semantic-ui-react';
 
 import Match from './presenter';
 
-const MatchContainer = () => <Match />;
+const MatchContainer = () => {
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleOnChange = (event: ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => {
+    const { value } = data;
+
+    setSearchInput(value);
+  };
+
+  const handleOnSearchClick = (event: FormEvent) => {
+    event.preventDefault();
+
+    console.log(`Searching for "${searchInput}"`);
+  };
+
+  return (
+    <Match
+      onChange={handleOnChange}
+      onSubmit={handleOnSearchClick}
+      searchInput={searchInput}
+    />
+  );
+};
 
 export default MatchContainer;
--- src/views/Match/presenter.tsx
+++ src/views/Match/presenter.tsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent } from 'react';
+import {
+  Icon,
+  Input,
+  InputOnChangeData,
+} from 'semantic-ui-react';
 
-const Match = () => (
-  <div>Match Page</div>
-);
+interface Props {
+  onChange: (event: ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => void;
+  onSubmit: (event: FormEvent) => void;
+  searchInput: string;
+}
+
+const Match = (props: Props) => {
+  const {
+    onChange,
+    onSubmit,
+    searchInput,
+  } = props;
+
+  const searchIcon = (
+    <Icon
+      circular
+      link
+      name="search"
+      onClick={onSubmit}
+    />
+  );
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <Input
+          icon={searchIcon}
+          onChange={onChange}
+          placeholder="Search"
+          value={searchInput}
+        />
+      </form>
+    </div>
+  );
+};
 
 export default Match;
