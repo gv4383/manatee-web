4d3d16d6efabe013a1d58d715a934103e531792b
gv4383 <gregory.vargas18@gmail.com>
2020-09-24 19:55:42 -0500

    Create SearchSkillState and SearchSkillEvent types

--- src/store/reducers/searchSkill/types.ts
+++ src/store/reducers/searchSkill/types.ts
@@ -5,3 +5,15 @@ export interface SearchSkillData {
   data: Skill[];
   included: User[];
 }
+
+export type SearchSkillState =
+  | { status: 'IDLE'; data: null; error: null }
+  | { status: 'LOADING'; data: SearchSkillData | null; error: null }
+  | { status: 'SUCCESS'; data: SearchSkillData; error: null }
+  | { status: 'FAILURE'; data: SearchSkillData | null; error: string };
+
+export interface SearchSkillEvent {
+  data: SearchSkillData;
+  error: string;
+  type: string;
+}
