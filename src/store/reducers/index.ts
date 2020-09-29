2835737a2b0dc1f2f3967ff16bd82955c00f9130
gv4383 <gregory.vargas18@gmail.com>
2020-09-29 09:00:42 -0500

    Add searchSkill to rootReducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,19 +1,23 @@
 import { combineReducers } from 'redux';
 
+import { SearchSkillState } from './searchSkill/types';
 import { SkillState } from './skill/types';
 import { UserState } from './user/types';
 import { UsersState } from './users/types';
+import searchSkill from './searchSkill';
 import skill from './skill';
 import user from './user';
 import users from './users';
 
 export interface AppState {
+  searchSkill: SearchSkillState;
   skill: SkillState;
   user: UserState;
   users: UsersState;
 }
 
 const rootReducer = combineReducers({
+  searchSkill,
   skill,
   user,
   users,
