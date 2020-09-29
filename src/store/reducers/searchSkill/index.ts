42cfce70939238be23a969f9631de8cc33822665
gv4383 <gregory.vargas18@gmail.com>
2020-09-28 19:56:59 -0500

    Create component reducers for SearchSkill reducer

---
+++ src/store/reducers/searchSkill/index.ts
@@ -0,0 +1,75 @@
+import { GET_SEARCH_SKILL } from '../../actions/searchSkill/constants';
+import { EVENTS, STATUSES } from '../../lib/constants';
+import { SearchSkillEvent, SearchSkillState } from './types';
+
+const initialSearchSkillState: SearchSkillState = {
+  status: STATUSES.IDLE,
+  data: null,
+  error: null,
+};
+
+const idleReducer = (
+  state: SearchSkillState,
+  event: SearchSkillEvent,
+) => {
+  const { type } = event;
+
+  switch (type) {
+    case `${GET_SEARCH_SKILL}/${EVENTS.FETCH}`:
+      return {
+        ...state,
+        status: STATUSES.LOADING,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+const loadingReducer = (
+  state: SearchSkillState,
+  event: SearchSkillEvent,
+) => {
+  const {
+    data,
+    error,
+    type,
+  } = event;
+
+  switch (type) {
+    case `${GET_SEARCH_SKILL}/${EVENTS.RESOLVE}`:
+      return {
+        status: STATUSES.SUCCESS,
+        data,
+        error: null,
+      };
+    case `${GET_SEARCH_SKILL}/${EVENTS.REJECT}`:
+      return {
+        ...state,
+        status: STATUSES.FAILURE,
+        error,
+      };
+    default:
+      return state;
+  }
+};
+
+const successFailureReducer = (
+  state: SearchSkillState,
+  event: SearchSkillEvent,
+) => {
+  const { type } = event;
+
+  switch (type) {
+    case `${GET_SEARCH_SKILL}/${EVENTS.FETCH}`:
+      return {
+        ...state,
+        status: STATUSES.LOADING,
+        error: null,
+      };
+    case `${GET_SEARCH_SKILL}/${EVENTS.CLEAR}`:
+      return initialSearchSkillState;
+    default:
+      return state;
+  }
+};
